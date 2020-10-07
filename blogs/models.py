3aa27c2b4fba6a3ffbb8764761d8950fbc0e60fc
unknown <yoyoanusingh27@gmail.com>
2020-10-07 21:07:55 +0530

    admin, blogs

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=60, default="TITLE")
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=20, default="Anonomous")
+    text_area = models.TextField(default="random text lorem ipsum lipsum jipsum kipsum hiphum jism jism 2")
+
+    def __str__(self):
+        return self.title
