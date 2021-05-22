962691dfa199a9d30fe8bc25035b07c28fd8bf7d
Kumar Anurag <yoyoanusingh27@gmail.com>
2021-05-22 20:11:12 +0530

    description, pic space, recent first

--- blogs/views.py
+++ blogs/views.py
@@ -5,7 +5,7 @@ from .models import Blog
 def index(request):
     queryset = Blog.objects.filter()
     context = {
-        'object_list': queryset
+        'object_list': queryset[::-1]
     }
     return render(request, 'blogs/index.html', context)
 
