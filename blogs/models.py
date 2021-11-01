e5d8c2dde4c99bc19c15f06d735c5b323cb3f66d
Kumar Anurag <yoyoanusingh27@gmail.com>
2021-11-01 15:23:55 +0530

    thumnails static working

--- blogs/models.py
+++ blogs/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from tinymce.models import HTMLField 
 
 class Blog(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=60, default="TITLE")
     date = models.DateTimeField(auto_now_add=True)
-    author = models.CharField(max_length=20, default="Anonomous")
-    text_area = models.TextField(default="random text lorem ipsum lipsum jipsum kipsum hiphum jism jism 2")
+    author = models.CharField(max_length=20, default="anurag")
+    description = models.CharField(max_length=100, default="wot???")
+    thumbnail = models.ImageField(blank=True, upload_to='static')
+    text_area = HTMLField()
+
 
     def yearpublished(self):
         return self.date.strftime('%b %d, %Y')
--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Blog
+import random
 #from django.http import HttpResponse
 
 def index(request):
     queryset = Blog.objects.filter()
+    seed = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
+    bg_color = ''.join(random.choice('0123456789ABCDEF') for i in range(6))
+    avatar_list = ['male', 'female', 'human', 'bottts', 'avataaars', 'jdenticon', 'gridy', 'micah', 'open-peeps', 'adventurer', 'big-smile']
     context = {
-        'object_list': queryset[::-1]
+        'object_list': queryset[::-1],
+        'avatar_sprites': avatar_list[random.randint(0, len(avatar_list)-1)],
+        'avatar_seed': seed,
+        'bg_color': bg_color
     }
     return render(request, 'blogs/index.html', context)
 
